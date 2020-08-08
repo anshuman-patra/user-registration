6c00a39d24d220ae6614f89c5577cb78ec6dd123
anshuman-patra <anshumanpatra845@gmail.com>
2020-08-08 20:25:19 +0530

    added user valid email id

--- userregistration.sh
+++ userregistration.sh
@@ -1,6 +1,8 @@
 #!/bin/bash 
 echo "welcome to user registration"
 pattern="^[[:upper:]]{1}[a-zA-Z]{3,}$"
+
+pattern1="^[a-zA-Z0-9]{3,15}(|[.|_|%|+|-]?[a-zA-Z0-9]+)@[a-zA-Z0-9]{1,15}(.[a-z]{2,4})(|[.]?[a-z]{2,4})$"
 read -p "Enter a First Name: " firstName
 if [[ $firstName =~ $pattern ]]
 then
@@ -14,4 +16,11 @@ then
 	echo "Valid"
 else
 	echo "Invalid"
+fi
+read -p "Enter a  valid Email : " email
+if [[ $email =~ $pattern1 ]]
+then
+	echo "Valid"
+else
+echo "Invalid"
 fi
