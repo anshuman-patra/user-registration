fd47502a96f9b01a584749e7773e7d7fa5d3c246
anshuman-patra <anshumanpatra845@gmail.com>
2020-08-08 20:42:35 +0530

    adding user phone number

--- userregistration.sh
+++ userregistration.sh
@@ -3,6 +3,7 @@ echo "welcome to user registration"
 pattern="^[[:upper:]]{1}[a-zA-Z]{3,}$"
 
 pattern1="^[a-zA-Z0-9]{3,15}(|[.|_|%|+|-]?[a-zA-Z0-9]+)@[a-zA-Z0-9]{1,15}(.[a-z]{2,4})(|[.]?[a-z]{2,4})$"
+pattern2="^[0-9]{1,4}[[:space:]][0-9]{10}$"
 read -p "Enter a First Name: " firstName
 if [[ $firstName =~ $pattern ]]
 then
@@ -24,3 +25,11 @@ then
 else
 echo "Invalid"
 fi
+read -p "Enter a phone number and also mention space betwwen  your country code : " phoneNumber
+if [[ $phoneNumber =~ $pattern2 ]]
+then
+	echo "Valid"
+else
+	echo "Invalid"
+fi
+
