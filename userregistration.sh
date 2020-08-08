96b304787722e04c3463db606601ff4f64fb9d57
anshuman-patra <anshumanpatra845@gmail.com>
2020-08-08 20:09:36 +0530

    enter valid first name for user

--- userregistration.sh
+++ userregistration.sh
@@ -1,3 +1,10 @@
 #!/bin/bash 
 echo "welcome to user registration"
-
+pattern="^[[:upper:]]{1}[a-zA-Z]{3,}$"
+read -p "Enter a First Name: " firstName
+if [[ $firstName =~ $pattern ]]
+then
+	echo "Valid"
+else
+	echo "Invalid"
+fi
